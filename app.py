cdaa923291267cad37454fc666fc25f00913a1e3
hsj <seungjune.hwang@gmail.com>
2021-05-24 16:45:14 +0900

    당신의 능력을 보여줘 추가 get post  데이터 전달

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from hello import money_pung
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +8,20 @@ def hello():
 
 @app.route('/money')
 def money():
-    return '돈을 입력하시오'
+    return render_template("money.html")
+
+@app.route('/show', methods=['GET', 'POST'])
+def show():
+    if request.method == 'GET':
+        return "Get 으로 들어온 페이지"
+    else:
+        # form 으로 전달된 데이터를 받아서 뻥튀기 해주야 함!!
+        money = request.form['money']
+        # money 의 값이 문자열이기 때문에 숫자로 변경
+        print("전달된 값은? ", int(money)*2)
+        im = money_pung(int(money))    # 뻥튀기 함수 사용
+        return "당신의 능력을 보여줘~ 얍<br><b>{}</b>원 드립니다".format(im)
+        # return "당신의 능력을 보여줘~ 얍<br><b>{}</b>원 드립니다".format(money_pung(int(money)))
 
 @app.route('/showmoney')
 def showmoney():
